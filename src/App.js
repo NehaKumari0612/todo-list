7bf111ccaa1ecf7c6c4b35cefce11e9d9cbacb98
Naveen <Naveen@LAPTOP-FGCAQQR5>
2021-06-18 14:31:52 +0530

    Add project files.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./MyComponent/Header";
+
+function App() {
+    let myVariable = 345;
+    return (
+        <>
+            <Header/>
+            
+
+        </>
+      
+
+  );
+}
+
+export default App;
